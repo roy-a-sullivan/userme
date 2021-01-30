c0f0572793d43d0f53256feb71859fa6ef7f4180
roy-a-sullivan <roysullivan30@gmail.com>
2021-01-30 16:08:04 -0500

    Adding generated Spring Boot Initializer app and dependencies.

---
+++ src/main/groovy/io/roysullivan/cooltings/userme/UsermeApplication.groovy
@@ -0,0 +1,13 @@
+package io.roysullivan.cooltings.userme
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class UsermeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(UsermeApplication, args)
+    }
+
+}
---
+++ src/test/groovy/io/roysullivan/cooltings/userme/UsermeApplicationTests.groovy
@@ -0,0 +1,13 @@
+package io.roysullivan.cooltings.userme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsermeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
