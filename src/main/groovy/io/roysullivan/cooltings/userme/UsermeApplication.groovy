c48dd9616a3f28db06f2f1ef35be5b97b732041b
roy-a-sullivan <roysullivan30@gmail.com>
2021-01-30 18:01:51 -0500

    initial setup and config.

--- src/main/groovy/io/roysullivan/cooltings/userme/UsermeApplication.groovy
+++ src/main/groovy/io/roysullivan/cooltings/userme/UsermeApplication.groovy
@@ -1,9 +1,20 @@
 package io.roysullivan.cooltings.userme
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
+import org.springframework.web.reactive.config.EnableWebFlux
 
-@SpringBootApplication
+@EnableWebFlux
+@EnableReactiveMongoRepositories
+@SpringBootApplication(
+        exclude=[
+            WebMvcAutoConfiguration.class,
+        ]
+)
 class UsermeApplication {
 
     static void main(String[] args) {
---
+++ src/main/groovy/io/roysullivan/cooltings/userme/security/SecurityGlobalConfig.groovy
@@ -0,0 +1,10 @@
+package io.roysullivan.cooltings.userme.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityGlobalConfig {
+    //...
+}
